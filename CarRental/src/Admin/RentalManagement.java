1cf394231da6fe187833ef830ef532df027f2369
Byron1001 <gradine702002@gmail.com>
2022-12-08 01:01:01 +0800

    Presave Rental Management

---
+++ CarRental/src/Admin/RentalManagement.java
@@ -0,0 +1,26 @@
+package Admin;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class RentalManagement {
+    private JTextField customerIDField;
+    private JTextField carIDField;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JPanel ButtonPanel;
+    private JPanel InfoPanel;
+    private JLabel customerIDLable;
+    private JLabel carIDLabel;
+
+    private JTable table1;
+
+    private static void createTable(ArrayList<Object[]> data){
+
+    }
+    public RentalManagement(){
+
+    }
+}
